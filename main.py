fd655a8fc904d45213726086258b513794b89f8c
amahlaka <arttu.mahlakaarto@gmail.com>
2017-05-24 01:11:15 +0300

    modified stuff

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ Tool to keep yourself aware of recent crypto events.
 
 Made by Arttu Mahlakaarto,
 """
-coins = "ZEC"
-def generateSpeech(text):
+
+oldZec = zecP = oldBtc = btcP = oldXmr = xmrP = oldEth = ethP = 0
+
+
+def generateSpeech(text, filename):
     from gtts import gTTS
 
     tts = gTTS(text=text, lang='en-us', slow=False)
-    tts.save("test.mp3")
+    tts.save(filename+".mp3")
     return
+
 
 def getCoin(coin, cu):
     """Get Value of coin in currency (cu)."""
@@ -20,36 +24,90 @@ def getCoin(coin, cu):
     output = format(response.json()[cu])
     return(output)
 
+
+from contextlib import contextmanager
+import sys, os
+
+
+@contextmanager
+def suppress_stdout():
+    with open(os.devnull, "w") as devnull:
+        old_stdout = sys.stdout
+        sys.stdout = devnull
+        try:
+            yield
+        finally:
+            sys.stdout = old_stdout
+
+
 def playSound(path):
     """Play the soundfile."""
 
     import time
     import vlc
     from mutagen.mp3 import MP3
-
-    audio = MP3(path)
-    length = audio.info.length
-    instance = vlc.Instance()
-    player = instance.media_player_new()
-    media = instance.media_new(path)
-    player.set_media(media)
-
-    player.play()
+    with suppress_stdout():
+        audio = MP3(path)
+        length = audio.info.length
+        instance = vlc.Instance()
+        player = instance.media_player_new()
+        media = instance.media_new(path)
+        player.set_media(media)
+        player.play()
     time.sleep(length)
 
     return
 
+def getDifference(old, new):
+    if(old == new):
+        return(0)
+    else:
+        difference = float(new)-float(old)
+        if(difference >= 0.5):
+            generateSpeech("DIFFERENCE OVER 0.5!", "warning")
+            playSound("warning.mp3")
+        return(difference)
+
+
+def colorize(inp):
+    from termcolor import colored
+    if(inp > 0):
+        color = "green"
+    if(inp < 0):
+        color = "red"
+    else:
+        color = "grey"
+    string = str("{:.5f}".format(inp))
+    print(colored(string, color))
+    colorizedString = colored(string, color)
+    return(colorizedString)
 import time
-coinvalue = getCoin(coins, "USD")
-generateSpeech("Current value for "+coins+" is:"+coinvalue+" USD")
-playSound("test.mp3")
-time.sleep(1)
-oldvalue = 250.3
+from datetime import datetime
 while True:
 
-    coinvalue = getCoin(coins, "USD")
-    diffe = str(round(abs(float(coinvalue)-float(oldvalue)),3))
-    generateSpeech(coins+" value: "+coinvalue+" USD, difference is: "+diffe)
-    playSound("test.mp3")
-    time.sleep(10)
-    oldvalue = coinvalue
+    zecP = getCoin("ZEC", "BTC")
+    btcP = getCoin("BTC", "USD")
+    xmrP = getCoin("XMR", "BTC")
+    ethP = getCoin("ETH", "BTC")
+    sp = "ZEC: "+zecP+". ETH: "+ethP+". XMR: "+xmrP+". BTC: "+btcP
+    zecDiff = getDifference(oldZec, zecP)
+    btcDiff = getDifference(oldBtc, btcP)
+    xmrDiff = getDifference(oldZec, xmrP)
+    ethDiff = getDifference(oldEth, ethP)
+    spacer = "     "
+    spacerb = "      "
+
+    td = colorize(zecDiff)
+    oldZec = zecP
+    oldBtc = btcP
+    oldXmr = xmrP
+    oldEth = ethP
+
+    print("-------------------------------------------------------------------")
+    print(sp+"  "+str(datetime.now().strftime('%H:%M:%S')))
+    print(td)
+    print("-------------------------------------------------------------------")
+    generateSpeech(sp, "forecast")
+    playSound("forecast.mp3")
+
+    time.sleep(5)
