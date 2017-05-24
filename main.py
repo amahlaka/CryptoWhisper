3d02385ab65b87d8b7dfa5b101b0f298f6882b1c
amahlaka <arttu.mahlakaarto@gmail.com>
2017-05-24 17:25:52 +0300

    Version 3

--- main.py
+++ main.py
@@ -4,12 +4,24 @@ Tool to keep yourself aware of recent crypto events.
 Made by Arttu Mahlakaarto,
 """
 
-oldZec = zecP = oldBtc = btcP = oldXmr = xmrP = oldEth = ethP = 0
+from contextlib import contextmanager
+import sys
+import os
+import time
+from datetime import datetime
+outp=""
+"""Edit only these 2 lines."""
+ZEC = {'name': 'ZEC', 'value': 0, 'oldValue': 0, 'difference': 0, 'color': 'white', 'currency': 'BTC'}
+ETH = {'name': 'ETH', 'value': 0, 'oldValue': 0, 'difference': 0, 'color': 'white', 'currency': 'BTC'}
+XMR = {'name': 'XMR', 'value': 0, 'oldValue': 0, 'difference': 0, 'color': 'white', 'currency': 'BTC'}
+BTC = {'name': 'BTC', 'value': 0, 'oldValue': 0, 'difference': 0, 'color': 'white', 'currency': 'USD'}
+"""Dont edit anything below"""
 
-
+worth = [0]
+difference = [0]
 def generateSpeech(text, filename):
+    """Generate the mp3 File using google api."""
     from gtts import gTTS
-
     tts = gTTS(text=text, lang='en-us', slow=False)
     tts.save(filename+".mp3")
     return
@@ -17,7 +29,6 @@ def generateSpeech(text, filename):
 
 def getCoin(coin, cu):
     """Get Value of coin in currency (cu)."""
-
     import requests
     re = "https://min-api.cryptocompare.com/data/price?fsym="+coin+"&tsyms="+cu
     response = requests.get(re)
@@ -25,12 +36,9 @@ def getCoin(coin, cu):
     return(output)
 
 
-from contextlib import contextmanager
-import sys, os
-
-
 @contextmanager
 def suppress_stdout():
+    """Silence the whining."""
     with open(os.devnull, "w") as devnull:
         old_stdout = sys.stdout
         sys.stdout = devnull
@@ -42,7 +50,6 @@ def suppress_stdout():
 
 def playSound(path):
     """Play the soundfile."""
-
     import time
     import vlc
     from mutagen.mp3 import MP3
@@ -58,59 +65,71 @@ def playSound(path):
 
     return
 
-def getDifference(old, new):
-    if(old == new):
-        return(0.000)
-    else:
-        difference = float(new)-float(old)
-        if(difference >= 0.5):
-            generateSpeech("DIFFERENCE OVER 0.5!", "warning")
-            playSound("warning.mp3")
-        return(difference)
+
+def colorize(dict):
+    """Return Colorized string."""
+    dif = dict['difference']
+    if(dif < 0.0000000000000000):
+        dict['color'] = "red"
+    if(dif > 0.00000000000000):
+        dict['color'] = "green"
+    if(dif == 0.000000000000000):
+        dict['color'] = "white"
 
 
-def colorize(inp):
+
+def generateStrings(dict):
     from termcolor import colored
-    if(inp > 0):
-        color = "green"
-    if(inp < 0):
-        color = "red"
-    if(inp == 0):
-        color = "grey"
-    strings = str("{:.5f}".format(inp))
-    colorizedString = colored(strings, color)
-    return(colorizedString)
-import time
-from datetime import datetime
+    outp = dict['name']+" "+str(dict['value'])+" change: "+colored(str(dict['difference']), dict['color'])+ " "
+
+    return(outp)
+
+
+# strings = str("{:.5f}".format(difference[x]))
+def printUpdate(val, diff):
+    """Print the latest update."""
+    clearScreen()
+    print("------------------------------------------------------------------")
+    print(val+"  "+str(datetime.now().strftime('%H:%M:%S')))
+    print(diff)
+    print("------------------------------------------------------------------")
+
+
+def clearScreen():
+    """Check os and clear the screen."""
+    import platform
+    import os
+    osystem = platform.system()
+
+    if(osystem == "Windows"):
+        os.system('cls')
+    if(osystem == "Linux"):
+        os.system('clear')
+    return
+
+
+def checkDifference(dict):
+    """Check Difference between old and new."""
+    dict['difference']=float(dict['value'])-float(dict['oldValue'])
+    colorize(dict)
+    dict['oldValue'] = dict['value']
+    return
+
+def checkValue(dict):
+    """Check value of all coins in list"""
+    dict['value']=float(getCoin(dict['name'],dict['currency']))
+    checkDifference(dict)
+    return
+
+
 while True:
 
-    zecP = getCoin("ZEC", "BTC")
-    btcP = getCoin("BTC", "USD")
-    xmrP = getCoin("XMR", "BTC")
-    ethP = getCoin("ETH", "BTC")
-    sp = "ZEC: "+zecP+". ETH: "+ethP+". XMR: "+xmrP+". BTC: "+btcP
-    zecDiff = getDifference(oldZec, zecP)
-    btcDiff = getDifference(oldBtc, btcP)
-    xmrDiff = getDifference(oldZec, xmrP)
-    ethDiff = getDifference(oldEth, ethP)
-    spacer = "     "
-    spacerb = "      "
-
-    zc = colorize(zecDiff)
-    bc = colorize(btcDiff)
-    xc = colorize(xmrDiff)
-    ec = colorize(ethDiff)
-    oldZec = zecP
-    oldBtc = btcP
-    oldXmr = xmrP
-    oldEth = ethP
-    td = spacer+zc+spacerb+ec+spacerb+xc+spacerb+bc
-    print("-------------------------------------------------------------------")
-    print(sp+"  "+str(datetime.now().strftime('%H:%M:%S')))
-    print(td)
-    print("-------------------------------------------------------------------")
-    generateSpeech(sp, "forecast")
-    with suppress_output():
-     playSound("forecast.mp3")
-
+    checkValue(ZEC)
+    pri = generateStrings(ZEC)
+    checkValue(ETH)
+    pri+=generateStrings(ETH)
+    checkValue(BTC)
+    pri+=generateStrings(BTC)
+    clearScreen()
+    print(pri)
     time.sleep(5)
